74f6779067284dd69cc6c12b9dfadd0d88d8753b
Poojani Ranasinghe <64425569+Poojani20@users.noreply.github.com>
2021-10-12 19:59:44 +0530

    Update main.c

--- calculator/main.c
+++ calculator/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int no1=10;
-	int no2=30;
-	 printf("sum =",(no1+no2));
+    int no2=30;
+	
+	int sum=no1+no2;
+	int sub=no1-no2;
+	int mul=no1*no2;
+	int div=no1/no2;
+	
+	 printf("Summation is =",sum);
+	 printf("Subtraction =",sub);
+	 printf("Multiplication is =",mul);
+	 printf("Division is =", div);
+	 
     return 0;
 }
   
